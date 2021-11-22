b1683ae769481cf56b12e81ba6431750b63d4ac7
leonaasanova <asanova_l@auca.kg>
2021-11-22 14:48:59 +0600

    commit

--- app/src/main/java/kg/test/midterm/MainActivity2.kt
+++ app/src/main/java/kg/test/midterm/MainActivity2.kt
@@ -1,11 +1,20 @@
 package kg.test.midterm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val next = findViewById<ImageButton>(R.id.next)
+
+        next.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
